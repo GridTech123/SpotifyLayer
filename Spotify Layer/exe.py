861cb08d91746b0812045b129d2f18084ab15502
GridTech <cdroach@live.com>
2017-02-26 09:22:13 -0600

    1.5.1 -Fixed installer and exe

--- Spotify Layer/exe.py
+++ Spotify Layer/exe.py
@@ -3,16 +3,4 @@ import cx_Freeze
 
 executables = [cx_Freeze.Executable('Spotify_Layer.py'), cx_Freeze.Executable('miniLayer.py')]
 
-cx_Freeze.setup(name = 'Spotify Layer', version = '1.4.0', options = {'build_exe': {'packages':['spotilib', 'pygame', 'pygame.locals', 'pickle', 'pyError', 'tkFileDialog'], 'include_files':["back.png",
-"background.png",
-"background2.png",
-"check.png",
-"close.png",
-"cross.png",
-"logo.png",
-"menuBack.png",
-"move.png",
-"pause.png",
-"settings.png",
-"smallWindow.png",
-"unpause.png"]}}, executables = executables)
+cx_Freeze.setup(name = 'Spotify Layer', version = '1.5.1', options = {'build_exe': {'packages':['spotilib', 'pygame', 'pygame.locals', 'pickle', 'pyError', 'tkFileDialog'], 'include_files':["images", 'backgrounds', 'backgroundImages']}}, executables = executables)
